ff9e29c82b4d710d8401055bb7cefa12e87dc421
AyeQG <ayesha.quinto01@estudiant.upf.edu>
2021-10-24 20:06:18 +0200

    added little

--- Lab1/P1Code/distancematrix/TestMatrix.java
+++ Lab1/P1Code/distancematrix/TestMatrix.java
@@ -6,12 +6,12 @@ public class TestMatrix {
         
         distanceMatrix.addCity( 100, 200, "Barcelona");
         distanceMatrix.addCity( 200, 50, "Madrid");
-        distanceMatrix.addCity( 99, 199, "BCN99");
+        distanceMatrix.addCity( 99, 200, "BCN99");
 
         System.out.println( distanceMatrix.getCityName(0) );
         System.out.println( distanceMatrix.getCityName(1) );
         
         System.out.println( distanceMatrix.getNoOfCities() );
-        System.out.println( distanceMatrix.getDistance(0, 1) );
+        System.out.println( distanceMatrix.getDistance(0, 2) );
     }
 }
