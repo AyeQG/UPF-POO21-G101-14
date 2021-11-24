c941285845d0c8e508d1b04b749fc48484031171
AyeQG <ayesha.quinto01@estudiant.upf.edu>
2021-11-24 18:58:17 +0100

    pushed minor changes

--- Lab4/P4Code/Color.java
+++ Lab4/P4Code/Color.java
@@ -4,16 +4,47 @@ import java.awt.Graphics;
 
 
 public class Color {
-    private double x;
-    private double y;
-    private double z;
+    private double x; //r, h
+    private double y; //g, s
+    private double z; //b, v
     private boolean isRGB;
     
     public Color( double xi, double yi, double zi, boolean isrgb ) {
         x = xi; y = yi; z = zi; isRGB = isrgb;
     }
     public void toHSV() {
+        double h, s, v;
+        double min, max, delta;
+        boolean converted = false;
 
+        min = Math.min(Math.min(x, y), z);
+        max = Math.max(Math.max(x, y), z);
+        while (converted == false) {
+            v = max; //V
+            delta = max - min;
+    
+            if( max != 0 ) { s = delta / max; } //S
+            else {
+                s = 0;
+                h = -1;
+                x = h; y = s; z = v;
+                converted = true;
+            }
+
+            if( x == max ) // H
+                h = ( y - z ) / delta; // between yellow & magenta
+            else if( y == max )
+                h = 2 + ( z - x ) / delta; // between cyan & yellow
+            else
+                h = 4 + ( x - y ) / delta; // between magenta & cyan
+            h *= 60;    // degrees
+    
+            if( h < 0 )
+                h += 360;
+            x = h; y = s; z = v;
+            converted = true;
+        }
+       
     }
     public void toRGB() {
         
--- Lab4/P4Code/TriangularRegion.java
+++ Lab4/P4Code/TriangularRegion.java
@@ -3,10 +3,10 @@ import java.awt.*;
 
 
 public class TriangularRegion extends PolygonalRegion {
-    
+
     
     public TriangularRegion( Point p1, Point p2, Point p3 ) {
-        super( new LinkedList<Point> ( new Point[]{p1, p2, p3} ) );
+        super( new LinkedList<Point> ( new Point[]{ p1, p2, p3} ) );
     }
     public double getArea() {
 
